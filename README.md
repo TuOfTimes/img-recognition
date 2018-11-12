4e085630fc84501a767a692d62196d8aa50ee592
Taha.Salman <taha.s147@gmail.com>
2018-11-12 18:22:32 -0500

    created data class

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Classify hand-drawn pictures
+# Image Recognition
+
+## Introduction
+In this project, we use a training set of 10,000 images to train multiple classifying models. Depending on the performance with the test data (also 10,000 images),
+we will pick the best model to compete in a Kaggle Competition.
+
+
+### Timeline
+
+* Friday 16th November: Done with Image Representation and Processing
+* Sunday 18th November: Done with implementation of atleast 2 algorithms
--- dataProcessor.py
+++ dataProcessor.py
@@ -1,2 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
 class Data():
-    pass
+    def __init__(self,filePath):
+        self.filePath = filePath
+        self.images = np.load(filePath,encoding='bytes')
+
+    def getImage(self,imageIndex,reshape=False):
+        if reshape:
+            return self.images[imageIndex][1].reshape(100,100)
+        return self.images[imageIndex][1]
+
+    def getClass(self,imageIndex):
+        return self.images[imageIndex][0]
+
+    def showImage(self,imageIndex):
+        img = self.getImage(imageIndex,True)
+        plt.imshow(img)
+
+if __name__ == "__main__":
+    data = Data("Data/train_images.npy")
+    print(data.getImage(1,True))
+    print(data.getClass(1))
+    data.showImage(1)
+
+
