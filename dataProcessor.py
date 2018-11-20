a944209577877ff92e8ce949c5aaa12bc2e64b58
Taha.Salman <taha.s147@gmail.com>
2018-11-19 21:55:34 -0500

    finalized image data handling

--- dataProcessor.py
+++ dataProcessor.py
@@ -5,20 +5,22 @@ from skimage.transform import resize
 
 class ImageProcessor():
 
-    def __init__(self,filePath,labelPath,categoryPath):
+    def __init__(self,filePath,labelPath=None,categoryPath=None):
         self.images = np.load(filePath,encoding='bytes')
         self.labels = []
-
-        with open(labelPath,"r") as f:      #store in list self.labels all the labels with index of list corresponding to index of image
-            data = f.readlines()
-        for i in range(1,len(data)):
-            self.labels.append(data[i].split(',')[1].strip("\n"))
         self.categories = {}
+        if labelPath and categoryPath:
+            with open(labelPath,"r") as f:      #store in list self.labels all the labels with index of list corresponding to index of image
+                data = f.readlines()
+            for i in range(1,len(data)):
+                self.labels.append(data[i].split(',')[1].strip("\n"))
+            self.categories = {}
 
-        with open(categoryPath,"r") as f:
-            data = f.readlines()
-        for i in range(0,len(data)):
-            self.categories[data[i].strip('\n')] = i
+            with open(categoryPath,"r") as f:
+                data = f.readlines()
+            for i in range(0,len(data)):
+                self.categories[data[i].strip('\n')] = i
+            print(self.categories)
 
     def getImage(self,imageIndex,reshape=False):
         if reshape:
@@ -96,19 +98,6 @@ class ImageProcessor():
                 if i<bbox[0] or i>bbox[2] or j<bbox[1] or j>bbox[3]:
                     img[i][j] = 0
 
-    # def getLabelledData(self):
-    #     '''
-    #     Returns a 2D array:
-    #     - 1st index -> class ID (0 - 31)
-    #     - 2nd index ->vector containing image data (10000 points)
-    #     '''
-    #     output = []
-    #     for i in range(0,len(self.labels)):
-    #         output.append([self.categories[self.labels[i]]])
-    #         output[i].append(self.images[i][1])
-    #     return output
-
-
     def getProcessedImages(self,mask_val=50,dims=(50,50),binaryRepresentation=False,min_area=100):
         processed_images = []
         for i in range(0,len(self.images)):
@@ -118,25 +107,46 @@ class ImageProcessor():
         return processed_images
 
     def labelImages(self,images):
+        if not self.categories or not self.labels:
+            return None
+
         output = []
         if len(images) != len(self.labels):
             return output
 
         for i in range(0,len(self.labels)):
-            output.append([self.categories[self.labels[i]]])
-            output[i].append(images[i])
+            temp = np.ndarray.tolist((images[i]))
+            temp = [self.categories[self.labels[i]]] + temp
+            output.append(np.asarray(temp))
+            # output.append([self.categories[self.labels[i]]])
+            # output[i].append(np.ndarray.tolist(images[i]))
         return output
 
     @staticmethod
-    def showImage(img):
+    def showImage(img,toReshape=False,reshape_dims =(100,100)):
+        if toReshape:
+            img = img.reshape(reshape_dims[0],reshape_dims[1])
         plt.imshow(img)
         plt.show()
 
-
 class ImageFileHandler():
-
-    def __init__(self,imagePath):
+    def __init__(self,imagePath,y_index=-1):
         self.images = np.load(imagePath,encoding="bytes")
+        self.yVector = []
+        self.xMatrix = self.images
+        if y_index == 0:
+            self.yVector = self._getYVector_(y_index)
+            self.xMatrix = self._getXVector_(y_index)
+
+    def _getYVector_(self,y_index):
+        y_vec = []
+        for i in range(0,len(self.images)):
+            y_vec.append(self.images[i][y_index])
+        return y_vec
+
+    def _getXVector_(self,y_index):
+        x_matrix = np.delete(self.images,y_index,1)
+        return x_matrix
 
     @staticmethod
     def saveNPYFile(img,img_path):
@@ -145,33 +155,45 @@ class ImageFileHandler():
             return
         np.save(img_path,img)
 
-if __name__ == "__main__":
+
+def processTrainingData():
     DATA_PATH = "Data/Raw/"
+    DATA_PROCESSED_PATH = "Data/Processed/"
 
-    img_processor = ImageProcessor(filePath=DATA_PATH+"train_images.npy",
-                                   labelPath=DATA_PATH+"train_labels.csv",
-                                   categoryPath=DATA_PATH+"categories.csv")
+    img_processor = ImageProcessor(filePath=DATA_PATH + "train_images.npy",
+                                   labelPath=DATA_PATH + "train_labels.csv",
+                                   categoryPath=DATA_PATH + "categories.csv")
 
-    processed_imgs = img_processor.getProcessedImages()
-    labelled_imgs = img_processor.labelImages(processed_imgs)
+    # Create Non-Binary representation
+    processed_imgs_nb = img_processor.getProcessedImages(mask_val=50,
+                                                         dims=(50, 50),
+                                                         binaryRepresentation=False,
+                                                         min_area=100)
 
-    ifm = ImageFileHandler.saveNPYFile(labelled_imgs,"Data/Processed/training_data.npy")
+    labelled_imgs_nb = img_processor.labelImages(processed_imgs_nb)
+    ifm = ImageFileHandler.saveNPYFile(labelled_imgs_nb, "Data/Processed/train_data_non_bin.npy")
+
+    # Create Binary Representation
+    processed_imgs_b = img_processor.getProcessedImages(mask_val=50,
+                                                        dims=(50, 50),
+                                                        binaryRepresentation=True,
+                                                        min_area=100)
+
+    labelled_imgs_b = img_processor.labelImages(processed_imgs_b)
+    ImageFileHandler.saveNPYFile(labelled_imgs_b, "Data/Processed/train_data_bin.npy")
+
+
+if __name__ == "__main__":
+    DATA_PROCESSED_PATH = "Data/Processed/"
+    #processTrainingData()
+
+
+    imf_nb = ImageFileHandler(imagePath=DATA_PROCESSED_PATH+"train_data_bin.npy",
+                              y_index=0)
+    img = imf_nb.xMatrix[18]
+    ImageProcessor.showImage(img,True,(50,50))
+
 
     # img = img_processor.getProcessedImage(imageIndex=0,mask_val=50,min_area=125)
     # img_processor.showImage(img)
-
-    #label_data = data.getLabelledData()
-    #data.showImageAtIndex(43)
-
-    #img = data.getImage(5,reshape=True)
-    # for i in range(0, len(img)):
-    #     if img[i] < 200:
-    #         img[i] = 0
-    #     else:
-    #         img[i] = 255
-    # img = img.reshape(100, 100)
-    # plt.imshow(img)
-    # plt.show()
-
-
 
