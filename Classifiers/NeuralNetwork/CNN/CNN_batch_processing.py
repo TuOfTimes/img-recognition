c396706375877850c74af22e728e724a9d1d2a9b
Edward <TuOfTimes@github.com>
2018-11-25 20:36:27 -0500

    added missing import

--- Classifiers/NeuralNetwork/CNN/CNN_batch_processing.py
+++ Classifiers/NeuralNetwork/CNN/CNN_batch_processing.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
-from keras.layers import Activation, Dropout, Flatten, Dense
+from keras.layers import Activation, Dropout, Flatten, Dense, BatchNormalization
 import logging
 
 '''
